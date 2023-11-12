86aa29f7d6fe95f3ee346d5943a2aad9d652c090
Hog <6062207@gmail.com>
2023-11-12 22:21:26 +0100

    feat:add to some changes to project in headers

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Store is mine!
           </Link>
         </Typography>
 
